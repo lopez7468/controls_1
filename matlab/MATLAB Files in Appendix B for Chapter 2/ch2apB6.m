d1fc1bddc33972857521f9332aa835e7cc176614
lopez7468 <60163629+lopez7468@users.noreply.github.com>
2023-02-21 00:44:41 -0800

    adds functions for analysis

--- matlab/MATLAB Files in Appendix B for Chapter 2/ch2apB6.m
+++ matlab/MATLAB Files in Appendix B for Chapter 2/ch2apB6.m
@@ -13,4 +13,4 @@
 P5=conv([1 7 10 9],[1 -3 6 2 1])    % Form (s^3+7s^2+10s+9)(s^4-
                                     % 3s^3+6s^2+2s+1), assign to 
                                     % P5, and display.
-	
+p2=conv([1 4],[1 1 4 2])
--- python_1/lti.py
+++ python_1/lti.py
@@ -2,14 +2,26 @@ from control.matlab import*
 from control import step_info
 import numpy as np
 import matplotlib.pyplot as plt
+from system_analysis import*
 
-num = [1,4]
-den = [1,1,4]
+num = [1, 4]
+den = [1,1,4,2]
+bruh =np.polymul([1, 4], [1,1,4,2])
+bruh=conv(num, den)
+
+print(bruh)
 sys = tf(num,den)
 print('G(s)= ', sys)
-den1=[1,1,4,0]
-sys1=tf(num,den1) # tf for ramp response
+
 Tf=15# plot range
 T=np.arange(0,Tf,0.1)
 
+#stability
+stability(num, den)
 
+#insert plots needed here
+#step_response(num, den)
+
+
+
+plt.show()
